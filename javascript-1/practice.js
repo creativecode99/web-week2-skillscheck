cc5de6b78f7bcacc9d4aca6839f4788b820a4e64
Ryan v <l9hfighter@gmail.com>
2019-08-15 16:59:55 -0500

    solution

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -2,42 +2,60 @@
 
 // Create a variable called 'myName' that's value is your name
 
-// Code here
+var myName = "Ryan";// Code here
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' that's value is your favorite number
 
-//Code here
+var faveNum = 432;//Code here
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and set it to true
 
-//Code here
+var lovesCode = true
+Boolean (lovesCode) //Code here
 
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, num1 and num2, and returns the sum of those two parameters.
 
-//Code here
+function sum (num1, num2){
+	return num1 + num2;
+}//Code here
 
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called x. Check to see if x is equal to true. If it is, return true as a boolean. If x does not equal true, return false as a boolean
 
-//Code here
+function lovesCodeChecker (x) {
+if (x > 0) {
+	return "true";
+} else {
+	return "false";
+}
+}//Code here
 
 //////////////////PROBLEM 6////////////////////
 
 // Create a function called 'oddChecker' that takes in one parameter, num. Check to see if the number is even or odd. If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
 
-//Code here
+function oddChecker(num) {
+if (value%2 == 0)
+
+ return true;
+else
+ return false;
+}//Code here
 
 //////////////////PROBLEM 7////////////////////
 
 // Create a function called 'iLove' that takes in two parameters, name which will be a string  and love which will be a string of something you love. Have the function take the two parameters and retrun a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
 
+function iLove ("name", "sushi"){
+	return "name" + sushi;
+}
 //Code Here
 
 //////////////////PROBLEM 8////////////////////
@@ -45,36 +63,66 @@
 // Create a copy of the faveColors array called 'colorCopy' using slice
 const faveColors = ['red', 'green', 'black']
 
+console.log(colorCopy.slice(0, 1, 2));
+
+var = ['red', 'green', 'black'];
+
 //Code Here
 
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push
 
-//Code Here
+var colorCopy = ['red', 'green', 'black'];
+array.push("green");//Code Here
 
 //////////////////PROBLEM 10////////////////////
 
 // Create a new array called 'middleNums' from the numbers array that will capture only the middle numbers (2, 3, 4).
 const numbers = [1, 2, 3, 4, 5]
 
-//Code Here
+var middleNums = array;
+array [1, 2, 3];//Code Here
 
 //////////////////PROBLEM 11////////////////////
 
 // Create an object called 'me' that has these keys with the correct values: firstName, state, age, greeter. firstName will be your name as a string. state will be your current state or providence as a string. age will be your age as a number. greeter will be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the correct values
 
+var me = {
+	firstName: "Ryan",
+	state: "Texas",
+	age: 28
+	greeter: function (){
+		return "Hello! My name is" + this.firstName + "and I live in" + this.state;
+	}
+
+};
 //Code Here
 
 //////////////////PROBLEM 12////////////////////
 
-// Create a function called 'bigOrSmall' that takes in one parameter, arr. Create a new array inside of bigOrSmall called 'answers'. Loop over the passed in arr param. If the number is GREATER than 100, push 'big' as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function
+// Create a function called 'bigOrSmall' that takes in one parameter, arr. 
+/*Create a new array inside of bigOrSmall called 'answers'. Loop over the passed in arr param. 
+If the number is GREATER than 100, push 'big' as a string to the answers array. 
+If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. 
+Return the answers array inside of the function */
+
+function bigOrSmall (arr){
+answers = []
+}
 
 // Code here
 
 //////////////////PROBLEM 13////////////////////
 
-// Create a function called 'arrayReverser' that takes in one parameter, arr. Inside of arrayReverser, create an empty array called 'reversed'. Using a for loop, loop over the passed in array in reverse and add each item to the new reversed array. Finally, return the new reversed array
+// Create a function called 'arrayReverser' that takes in one parameter, arr. 
+/*Inside of arrayReverser, create an empty array called 'reversed'. 
+Using a for loop, loop over the passed in array in reverse and add each item to the new reversed array. 
+Finally, return the new reversed array*/
+
+function arrayReverser (arr){
+	reversed = []
+}
 
 //Code Here
 
@@ -88,7 +136,7 @@ const myNumbers = [3, 55, 788, 2, 1]
 
 // First, let's use .map to create a new array that doubles each number. Call the new array 'doubled'
 
-// Code Here
+const map1 = array1.map(x => x * 2);// Code Here
 
 //////////////////PROBLEM 15////////////////////
 
--- javascript-3/student.js
+++ javascript-3/student.js
@@ -1,34 +1,49 @@
 //////////////////SETUP////////////////////
-// Close all other index.html files and open the index.html file in javascript-3/index.html. You should see an input and button that says "Add Item"
+// Close all other index.html files and open the index.html file in javascript-3/index.html. 
+//You should see an input and button that says "Add Item"
 
 //////////////////STEP 1////////////////////
 //Create an array called 'groceries' that contains three grocery items as a string
 
-// Code Here
+var groceries = ["butter", "watermelon", "rice", "param"];// Code Here
 
 //////////////////STEP 2////////////////////
 //Create a variable called 'yourName' that is equal to your name as a string
 
-// Code Here
+var yourName = "Ryan"; // Code Here
 
 //////////////////STEP 3////////////////////
-//Create a function called 'setGroceryListTitle' that takes in one parameter called x. The function should return a string of 'XPARAM's Grocery List'.
-
+//Create a function called 'setGroceryListTitle' that takes in one parameter called x. 
+//The function should return a string of 'XPARAM's Grocery List'.
+function setGroceryListTitle(x){
+    return XPARAM;
+}
 // Code Here
 
 //////////////////STEP 4////////////////////
-//Create a function called 'addItem' that takes in one paramter called item. Add the item param to the end of the groceries array created above. Inside of addItem, invoke displayData() which is a function we created that takes care of some of the leg work unrelated to this module.
+//Create a function called 'addItem' that takes in one paramter called item. 
+/*Add the item param to the end of the groceries array created above. 
+Inside of addItem, invoke displayData() which is a function we 
+created that takes care of some of the leg work unrelated to this module.*/
 
-// Code Here
+function addItem(item){
+return displayData().item
+}// Code Here
 
 //////////////////STEP 5////////////////////
-//Create a function called removeItem that takes in one parmater called index. Inside of the function remove the item from the groceries array above using splice. Invoke the displayData() function again.
+//Create a function called removeItem that takes in one parmater called index. 
+/*Inside of the function remove the item from the groceries array above using splice. 
+Invoke the displayData() function again.*/
 
-// Code Here
+function removeItem(index){
+    
+}// Code Here
 
 //////////////////STEP 6////////////////////
 //Create a function called 'checkGroceryCount'. Inside of the function check the total number of groceries you need to get using .length. Using an else/if statement, check if the number is greater than or equal to 5, return the string 'That looks like a big trip' from the function. If the number of groceries is equal to one, return the string '1 item'. For numbers between 1 and 5, return the string 'NUMBER items' with number replacing the number of items
+function checkGroceryCount(){
 
+}
 // Code Here
 
 //////////////////STEP 6////////////////////
